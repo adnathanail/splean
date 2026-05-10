799dc1cf4def9b800a6042a8516b4b4fa0eed7da
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-05-10 17:30:16 +0200

    Write out exercise 3.7 in algebraic

--- Main.lean
+++ Main.lean
@@ -155,6 +155,17 @@ def algStack : ZX 2 2 := .wire ⊗ .hadamard
 def algCnot : ZX 2 2 := (.spider .Z 1 2 ⊗ .wire) × (.wire ⊗ .spider .X 2 1)
 #html algCnot.toHtml
 
+def algNotc : ZX 2 2 := (.spider .X 1 2 ⊗ .wire) × (.wire ⊗ .spider .Z 2 1)
+#html algNotc.toHtml
+
+def algCx : ZX 2 2 :=
+  (
+    (.spider .Z 1 2 ⊗ .wire) ×
+    (.wire ⊗ .hadamard ⊗ .wire)
+  ) ×
+  (.wire ⊗ .spider .Z 2 1)
+#html algCx.toHtml
+
 def algLayoutTest1 : ZX 4 4 := algCnot ⊗ algCnot
 #html algLayoutTest1.toHtml
 
@@ -168,3 +179,12 @@ def algLayoutTest4a : ZX 2 4 := (.spider .Z 1 3 ⊗ .wire)
 def algLayoutTest4b : ZX 4 2 := (.wire ⊗ .spider .Z 3 1)
 def algLayoutTest4 : ZX 2 2 := (.spider .Z 1 3 ⊗ .wire) × (.wire ⊗ .spider .X 3 1)
 #html algLayoutTest4.toHtml
+
+def algExercise3point7a : ZX 2 3 :=
+  (.wire ⊗ .wire ⊗ .spider .Z 0 1) ×
+  (.wire ⊗ algNotc)
+def algExercise3point7b : ZX 3 3 := .wire ⊗ .hadamard ⊗ .spider .X 1 1 ⟨1, 1⟩
+def algExercise3point7c : ZX 3 2 := (algNotc ⊗ .spider .X 1 0)
+def algExercise3point7d : ZX 2 2 := (.wire ⊗ .hadamard) × algCx
+def algExercise3point7 : ZX 2 2 := ((algExercise3point7a × algExercise3point7b) × algExercise3point7c) × algExercise3point7d
+#html algExercise3point7.toHtml
