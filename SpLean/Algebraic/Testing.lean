adnathanail/LeanSpider

Replace Phase type with MathLib's Q for ease of arithmetic

LeanSpider/Algebraic/Testing.lean
@@ -1,4 +1,4 @@
-import LeanSpider.All
+import LeanSpider.Algebraic
 
 open LeanSpider.Algebraic
 
@@ -8,20 +8,21 @@ open LeanSpider.Algebraic
 
 -- Algebraic-ZX terms can be rendered
 open LeanSpider.Algebraic
-def algSpider : ZX 1 1 := .spider .Z 1 1 ⟨1, 2⟩
+def algSpider : ZX 1 1 := .spider .Z 1 1 (phaseLit 1 2)
 #html algSpider.toHtml
 
 -- Example spider fusion proof
-abbrev algFusionLHS : ZX 1 1 := .spider .Z 1 1 ⟨1, 4⟩ × .spider .Z 1 1 ⟨1, 4⟩
+abbrev algFusionLHS : ZX 1 1 :=
+  .spider .Z 1 1 (phaseLit 1 4) × .spider .Z 1 1 (phaseLit 1 4)
 #html algFusionLHS.toHtml
 
-abbrev algFusionRHS : ZX 1 1 := .spider .Z 1 1 ⟨1, 2⟩
+abbrev algFusionRHS : ZX 1 1 := .spider .Z 1 1 (phaseLit 1 2)
 #html algFusionRHS.toHtml
 
 theorem algFusion : algFusionLHS ≃ZX algFusionRHS := by
   show _ = _
   rw [Z_spiderFusion]
-  exact spider_phase_eq (congr_phase (by decide))
+  exact spider_eq_of_phase_eq (by unfold phaseLit; norm_num)
 
 def algLayoutTest1 : ZX 4 4 := GateCNOT ⊗ GateCNOT
 #html algLayoutTest1.toHtml
@@ -40,74 +41,99 @@ def algLayoutTest4 : ZX 2 2 := (.spider .Z 1 3 ⊗ .wire) × (.wire ⊗ .spider 
 def algExercise3point7a : ZX 2 3 :=
   (.wire ⊗ .wire ⊗ .spider .Z 0 1) ×
   (.wire ⊗ GateNOTC)
-def algExercise3point7b : ZX 3 3 := .wire ⊗ .hadamard ⊗ .spider .X 1 1 ⟨1, 1⟩
+def algExercise3point7b : ZX 3 3 :=
+  .wire ⊗ .hadamard ⊗ .spider .X 1 1 (phaseLit 1 1)
 def algExercise3point7c : ZX 3 2 := (GateNOTC ⊗ .spider .X 1 0)
 def algExercise3point7d : ZX 2 2 := (.wire ⊗ .hadamard) × GateCX
-def algExercise3point7 : ZX 2 2 := ((algExercise3point7a × algExercise3point7b) × algExercise3point7c) × algExercise3point7d
+def algExercise3point7 : ZX 2 2 :=
+  ((algExercise3point7a × algExercise3point7b) × algExercise3point7c) × algExercise3point7d
 #html algExercise3point7.toHtml
 
 /-! ## `zx_alg_fusion` — graph-style spider fusion tactic
 
     Apply Z-spider fusion at two named node IDs. The IDs match those
     assigned by `ZX.toPositionedDiagram`, so they are the same numbers
-    you see in the rendered diagram.
-
-    Scope: direct-compose only. Both spiders must be Z, with arities
-    `(_, 1)` and `(1, _)`, and sit as the immediate children of a single
-    `compose`. The fused result has the *raw* phase sum (no
-    simplification) — phase simplification stays a separate concern. -/
+    you see in the rendered diagram. Phase residuals are left raw
+    (`α + β`) — close them with `decide` / `norm_num` / `abel` / `ring`
+    depending on shape. -/
 
 -- (1) Top-level fusion with concrete phases. IDs: s1=0, s2=1.
---     The tactic gcd-reduces the summed phase, so ⟨1,4⟩+⟨1,4⟩ = ⟨8,16⟩
---     becomes ⟨1,2⟩.
+--     The fused residual phase is `phaseLit 1 4 + phaseLit 1 4`; closed
+--     against `phaseLit 1 2` by `decide` (closed `ℚ` equality is decidable).
 theorem zxAlgFusion_topLevel :
-    (ZX.spider .Z 1 1 ⟨1, 4⟩ × ZX.spider .Z 1 1 ⟨1, 4⟩)
-      ≃ZX ZX.spider .Z 1 1 ⟨1, 2⟩ := by
+    (ZX.spider .Z 1 1 (phaseLit 1 4) × ZX.spider .Z 1 1 (phaseLit 1 4))
+      ≃ZX ZX.spider .Z 1 1 (phaseLit 1 2) := by
   zx_alg_fusion 0 1
-  rfl
+  exact spider_eq_of_phase_eq (by unfold phaseLit; norm_num)
 
-theorem zxAlgFusion_parameterized (α β : Phase) :
+theorem zxAlgFusion_parameterized (α β : AlgPhase) :
     (ZX.spider .Z 1 1 α × ZX.spider .Z 1 1 β)
       ≃ZX ZX.spider .Z 1 1 (α + β) := by
   zx_alg_fusion 0 1
   rfl
 
-theorem zxAlgFusion_parameterized2 (α : Phase) :
-    (ZX.spider .Z 1 1 α × ZX.spider .Z 1 1 ⟨1, 2⟩)
-      ≃ZX ZX.spider .Z 1 1 (α + ⟨1, 2⟩) := by
+theorem zxAlgFusion_parameterized2 (α : AlgPhase) :
+    (ZX.spider .Z 1 1 α × ZX.spider .Z 1 1 (phaseLit 1 2))
+      ≃ZX ZX.spider .Z 1 1 (α + phaseLit 1 2) := by
   zx_alg_fusion 0 1
   rfl
 
-def zaf_pa3a (α : Phase) : ZX 1 1 := (ZX.spider .Z 1 1 α × ZX.spider .Z 1 1 (-α))
-theorem zxAlgFusion_parameterized3a (α : Phase) :
-    zaf_pa3a α
-      ≃ZX ZX.spider .Z 1 1 := by
+def zaf_pa3a (α : AlgPhase) : ZX 1 1 :=
+  ZX.spider .Z 1 1 α × ZX.spider .Z 1 1 (-α)
+theorem zxAlgFusion_parameterized3a (α : AlgPhase) :
+    zaf_pa3a α ≃ZX ZX.spider .Z 1 1 := by
   zx_alg_fusion 0 1
-  zx_alg_phase_cancel 0
-  rfl
+  exact spider_eq_of_phase_eq (by abel)
 
-def zaf_pa3b (α : Phase) : ZX 1 1 := zaf_pa3a α × ZX.spider .Z 1 1 ⟨1, 2⟩
-theorem zxAlgFusion_parameterized3b (α : Phase) :
+def zaf_pa3b (α : AlgPhase) : ZX 1 1 := zaf_pa3a α × ZX.spider .Z 1 1 (phaseLit 1 2)
+theorem zxAlgFusion_parameterized3b (α : AlgPhase) :
     zaf_pa3b α
-      ≃ZX (ZX.spider .Z 1 1 × ZX.spider .Z 1 1 ⟨1, 2⟩) := by
+      ≃ZX (ZX.spider .Z 1 1 × ZX.spider .Z 1 1 (phaseLit 1 2)) := by
   zx_alg_fusion 0 1
-  zx_alg_phase_cancel 0
-  rfl
+  refine ZX.compose_congr ?_ (ZX.equiv_refl _)
+  exact spider_eq_of_phase_eq (by abel)
+
+def zaf_pa3c (α : AlgPhase) : ZX 1 1 := zaf_pa3a α × ZX.spider .Z 1 1 α
+theorem zxAlgFusion_parameterized3c (α : AlgPhase) :
+    zaf_pa3c α ≃ZX ZX.spider .Z 1 1 α := by
+  zx_alg_fusion 0 1
+  zx_alg_fusion 0 1
+  exact spider_eq_of_phase_eq (by abel)
+
+/-! ### Headline win: full abelian-group reasoning on phases
+
+    With `AlgPhase = ℚ`, any polynomial phase identity reduces to ℚ algebra
+    and closes by `abel` / `ring` / `decide`. No `congr_phase`, no `decide`
+    on integer cross-multiplications. -/
+
+example (α : AlgPhase) :
+    ZX.spider .Z 1 1 (α + (-α) + α) ≃ZX ZX.spider .Z 1 1 α :=
+  spider_eq_of_phase_eq (by abel)
+
+example (α β : AlgPhase) :
+    ZX.spider .Z 1 1 (α + β + (-α)) ≃ZX ZX.spider .Z 1 1 β :=
+  spider_eq_of_phase_eq (by abel)
+
+example (α : AlgPhase) :
+    ZX.spider .Z 1 1 (2 * α - α) ≃ZX ZX.spider .Z 1 1 α :=
+  spider_eq_of_phase_eq (by ring)
 
 -- (2) Nested under a surrounding compose. IDs (DFS):
 --   s1=0 (outer left), s2=1 (target left), s3=2 (target right), s4=3 (outer right).
 theorem zxAlgFusion_nested :
     ZX.compose
-        (ZX.compose (ZX.spider .Z 1 1 ⟨1, 8⟩)
-          (ZX.compose (ZX.spider .Z 1 1 ⟨1, 4⟩) (ZX.spider .Z 1 1 ⟨1, 4⟩)))
-        (ZX.spider .Z 1 1 ⟨1, 8⟩)
+        (ZX.compose (ZX.spider .Z 1 1 (phaseLit 1 8))
+          (ZX.compose (ZX.spider .Z 1 1 (phaseLit 1 4))
+                      (ZX.spider .Z 1 1 (phaseLit 1 4))))
+        (ZX.spider .Z 1 1 (phaseLit 1 8))
       ≃ZX
     ZX.compose
-        (ZX.spider .Z 1 1 (⟨5, 8⟩))
-        (ZX.spider .Z 1 1 ⟨1, 8⟩) := by
+        (ZX.spider .Z 1 1 (phaseLit 5 8))
+        (ZX.spider .Z 1 1 (phaseLit 1 8)) := by
   zx_alg_fusion 1 2
   zx_alg_fusion 0 1
-  rfl
+  refine ZX.compose_congr ?_ (ZX.equiv_refl _)
+  exact spider_eq_of_phase_eq (by unfold phaseLit; norm_num)
 
 -- Axiom audit: proofs built by the tactic depend only on the standard
 -- three axioms — no project-local soundness axioms.

LeanSpider/Algebraic/ZX.lean
@@ -1,14 +1,17 @@
 import LeanSpider.ZXDiagram
+import LeanSpider.Algebraic.Phase
 
 namespace LeanSpider.Algebraic
 
 /-- A free-algebra ZX term, indexed by its arity (`n` inputs, `m` outputs).
-    Reuses `SpiderColor` and `Phase` from the graph-style `ZXDiagram` module. -/
+    Reuses `SpiderColor` from the graph-style `ZXDiagram` module; phases are
+    `AlgPhase = ℚ` (see `LeanSpider/Algebraic/Phase.lean`) so phase algebra
+    obeys `AddCommGroup` laws directly. -/
 inductive ZX : Nat → Nat → Type
   | empty    : ZX 0 0
   | wire     : ZX 1 1
   | hadamard : ZX 1 1
-  | spider   (c : SpiderColor) (n m : Nat) (φ : Phase := ⟨0, 1⟩) : ZX n m
+  | spider   (c : SpiderColor) (n m : Nat) (φ : AlgPhase := 0) : ZX n m
   | stack    {n m p q : Nat} : ZX n m → ZX p q → ZX (n + p) (m + q)
   | compose  {n m k : Nat} : ZX n m → ZX m k → ZX n k
 

Tests/PhaseLabel.lean
@@ -2,13 +2,14 @@ import LSpec
 import LeanSpider.Algebraic.Tactics
 
 /-! Unit tests for `LeanSpider.Algebraic.phaseExprToLabel` — the MetaM
-    walker that converts a `Phase`-typed `Expr` into its display string.
+    walker that converts an `AlgPhase`-typed `Expr` into its display string.
     Tests run at compile time via `#guard_msgs in #eval`; any divergence
     from the expected output fails the `lake build Tests` run.
 
-    Covers: concrete literals (gcd + mod-2π normalization via
-    `Phase.simplify`), surface `HAdd` / `Phase.add` combinators, free
-    variables, and mixed symbolic-plus-concrete expressions.
+    Covers: concrete `phaseLit` literals (gcd + mod-2π normalization happens
+    in the graph-side bridge `AlgPhase.toGraphPhase` + `Phase.simplify`),
+    surface `HAdd` / `Neg` combinators, free variables, and mixed
+    symbolic-plus-concrete expressions.
 
     These tests don't go through the LSpec runner because
     `phaseExprToLabel` is `MetaM`-bound and `LSpec.test` only accepts
@@ -18,12 +19,9 @@ open Lean Elab Meta LeanSpider.Algebraic
 
 namespace LeanSpider.Tests.PhaseLabel
 
-/-- Build a `Phase.mk n d` `Expr` (used as a literal building block). -/
-private def phaseLit (n : Int) (d : Nat) : MetaM Expr := do
-  let numE : Expr := Lean.toExpr n
-  let denE ← mkAppOptM ``OfNat.ofNat
-                #[some (mkConst ``PNat), some (Lean.toExpr d), none]
-  mkAppM ``Phase.mk #[numE, denE]
+/-- Build a `LeanSpider.Algebraic.phaseLit p q` `Expr` of type `AlgPhase`. -/
+private def phaseLit (p q : Int) : MetaM Expr := do
+  mkAppM ``LeanSpider.Algebraic.phaseLit #[Lean.toExpr p, Lean.toExpr q]
 
 -- == Concrete literals ==
 
@@ -43,74 +41,69 @@ private def phaseLit (n : Int) (d : Nat) : MetaM Expr := do
 #guard_msgs in #eval show MetaM String from do
   phaseExprToLabel (← phaseLit 2 3)
 
--- `Phase.simplify` gcd-reduces ⟨2, 4⟩ to ⟨1, 2⟩.
+-- ℚ auto-reduces 2/4 → 1/2; formatted as "π/2".
 /-- info: "π/2" -/
 #guard_msgs in #eval show MetaM String from do
   phaseExprToLabel (← phaseLit 2 4)
 
--- `Phase.simplify` mod-2π reduces ⟨5, 2⟩ → ⟨1, 2⟩ (since 5/2 ≡ 1/2 mod 2).
+-- `Phase.simplify` (in the graph-side display bridge) mod-2π reduces 5/2 → 1/2.
 /-- info: "π/2" -/
 #guard_msgs in #eval show MetaM String from do
   phaseExprToLabel (← phaseLit 5 2)
 
--- Euclidean mod in Lean: -1 % 8 = 7, so ⟨-1, 4⟩ formats as 7π/4
--- (the same angle on the circle, just expressed with non-negative
--- numerator). Documents the simplify-on-format behaviour.
+-- Lean's Euclidean mod gives -1 % 8 = 7, so the angle prints as 7π/4.
 /-- info: "7π/4" -/
 #guard_msgs in #eval show MetaM String from do
   phaseExprToLabel (← phaseLit (-1) 4)
 
 -- == Surface combinators on closed phases ==
---    Crucial: must NOT unfold to `Phase.mk (a*d + c*b) (b*d)` —
---    that was the bug from the previous iteration of this walker.
+--    Closed sums get evaluated as `ℚ` and rendered as a single literal:
+--    `phaseLit 1 4 + phaseLit 1 4` simplifies to `π/2`.
 
-/-- info: "π/4 + π/4" -/
+/-- info: "π/2" -/
 #guard_msgs in #eval show MetaM String from do
   let a ← phaseLit 1 4
   let b ← phaseLit 1 4
   phaseExprToLabel (← mkAppM ``HAdd.hAdd #[a, b])
 
--- Direct `Phase.add` (in case some caller bypasses the typeclass).
-/-- info: "π/4 + π/4" -/
-#guard_msgs in #eval show MetaM String from do
-  let a ← phaseLit 1 4
-  let b ← phaseLit 1 4
-  phaseExprToLabel (← mkAppM ``Phase.add #[a, b])
-
 -- == Symbolic free variables ==
 
 /-- info: "α" -/
 #guard_msgs in #eval show MetaM String from do
-  withLocalDeclD `α (mkConst ``Phase) fun α =>
+  withLocalDeclD `α (mkConst ``AlgPhase) fun α =>
     phaseExprToLabel α
 
 /-- info: "α + β" -/
 #guard_msgs in #eval show MetaM String from do
-  withLocalDeclD `α (mkConst ``Phase) fun α =>
-    withLocalDeclD `β (mkConst ``Phase) fun β => do
+  withLocalDeclD `α (mkConst ``AlgPhase) fun α =>
+    withLocalDeclD `β (mkConst ``AlgPhase) fun β => do
       phaseExprToLabel (← mkAppM ``HAdd.hAdd #[α, β])
 
 -- == Mixed: symbolic + concrete ==
---    The original bug case — these previously printed
---    `α + { num := 1, den := 2 }`.
 
 /-- info: "α + π/2" -/
 #guard_msgs in #eval show MetaM String from do
-  withLocalDeclD `α (mkConst ``Phase) fun α => do
+  withLocalDeclD `α (mkConst ``AlgPhase) fun α => do
     phaseExprToLabel (← mkAppM ``HAdd.hAdd #[α, ← phaseLit 1 2])
 
 /-- info: "π/2 + α" -/
 #guard_msgs in #eval show MetaM String from do
-  withLocalDeclD `α (mkConst ``Phase) fun α => do
+  withLocalDeclD `α (mkConst ``AlgPhase) fun α => do
     phaseExprToLabel (← mkAppM ``HAdd.hAdd #[← phaseLit 1 2, α])
 
 -- Nested combinators recurse correctly.
 /-- info: "α + β + π/2" -/
 #guard_msgs in #eval show MetaM String from do
-  withLocalDeclD `α (mkConst ``Phase) fun α =>
-    withLocalDeclD `β (mkConst ``Phase) fun β => do
+  withLocalDeclD `α (mkConst ``AlgPhase) fun α =>
+    withLocalDeclD `β (mkConst ``AlgPhase) fun β => do
       let inner ← mkAppM ``HAdd.hAdd #[α, β]
       phaseExprToLabel (← mkAppM ``HAdd.hAdd #[inner, ← phaseLit 1 2])
+
+-- Unary minus on a free variable.
+/-- info: "-α" -/
+#guard_msgs in #eval show MetaM String from do
+  withLocalDeclD `α (mkConst ``AlgPhase) fun α => do
+    phaseExprToLabel (← mkAppM ``Neg.neg #[α])
 
 -- A trivial `TestSeq` so that `Tests/All.lean`'s aggregate has a stable
 -- entry for this file. The real assertions are the `#guard_msgs` blocks

adnathanail/splean

SpLean/Algebraic/Testing.lean
@@ -1,4 +1,4 @@
-import SpLean.All
+import SpLean.Algebraic
 
 open SpLean.Algebraic
 
@@ -8,20 +8,21 @@ open SpLean.Algebraic
 
 -- Algebraic-ZX terms can be rendered
 open SpLean.Algebraic
-def algSpider : ZX 1 1 := .spider .Z 1 1 ⟨1, 2⟩
+def algSpider : ZX 1 1 := .spider .Z 1 1 (phaseLit 1 2)
 #html algSpider.toHtml
 
 -- Example spider fusion proof
-abbrev algFusionLHS : ZX 1 1 := .spider .Z 1 1 ⟨1, 4⟩ × .spider .Z 1 1 ⟨1, 4⟩
+abbrev algFusionLHS : ZX 1 1 :=
+  .spider .Z 1 1 (phaseLit 1 4) × .spider .Z 1 1 (phaseLit 1 4)
 #html algFusionLHS.toHtml
 
-abbrev algFusionRHS : ZX 1 1 := .spider .Z 1 1 ⟨1, 2⟩
+abbrev algFusionRHS : ZX 1 1 := .spider .Z 1 1 (phaseLit 1 2)
 #html algFusionRHS.toHtml
 
 theorem algFusion : algFusionLHS ≃ZX algFusionRHS := by
   show _ = _
   rw [Z_spiderFusion]
-  exact spider_phase_eq (congr_phase (by decide))
+  exact spider_eq_of_phase_eq (by unfold phaseLit; norm_num)
 
 def algLayoutTest1 : ZX 4 4 := GateCNOT ⊗ GateCNOT
 #html algLayoutTest1.toHtml
@@ -40,74 +41,99 @@ def algLayoutTest4 : ZX 2 2 := (.spider .Z 1 3 ⊗ .wire) × (.wire ⊗ .spider 
 def algExercise3point7a : ZX 2 3 :=
   (.wire ⊗ .wire ⊗ .spider .Z 0 1) ×
   (.wire ⊗ GateNOTC)
-def algExercise3point7b : ZX 3 3 := .wire ⊗ .hadamard ⊗ .spider .X 1 1 ⟨1, 1⟩
+def algExercise3point7b : ZX 3 3 :=
+  .wire ⊗ .hadamard ⊗ .spider .X 1 1 (phaseLit 1 1)
 def algExercise3point7c : ZX 3 2 := (GateNOTC ⊗ .spider .X 1 0)
 def algExercise3point7d : ZX 2 2 := (.wire ⊗ .hadamard) × GateCX
-def algExercise3point7 : ZX 2 2 := ((algExercise3point7a × algExercise3point7b) × algExercise3point7c) × algExercise3point7d
+def algExercise3point7 : ZX 2 2 :=
+  ((algExercise3point7a × algExercise3point7b) × algExercise3point7c) × algExercise3point7d
 #html algExercise3point7.toHtml
 
 /-! ## `zx_alg_fusion` — graph-style spider fusion tactic
 
     Apply Z-spider fusion at two named node IDs. The IDs match those
     assigned by `ZX.toPositionedDiagram`, so they are the same numbers
-    you see in the rendered diagram.
-
-    Scope: direct-compose only. Both spiders must be Z, with arities
-    `(_, 1)` and `(1, _)`, and sit as the immediate children of a single
-    `compose`. The fused result has the *raw* phase sum (no
-    simplification) — phase simplification stays a separate concern. -/
+    you see in the rendered diagram. Phase residuals are left raw
+    (`α + β`) — close them with `decide` / `norm_num` / `abel` / `ring`
+    depending on shape. -/
 
 -- (1) Top-level fusion with concrete phases. IDs: s1=0, s2=1.
---     The tactic gcd-reduces the summed phase, so ⟨1,4⟩+⟨1,4⟩ = ⟨8,16⟩
---     becomes ⟨1,2⟩.
+--     The fused residual phase is `phaseLit 1 4 + phaseLit 1 4`; closed
+--     against `phaseLit 1 2` by `decide` (closed `ℚ` equality is decidable).
 theorem zxAlgFusion_topLevel :
-    (ZX.spider .Z 1 1 ⟨1, 4⟩ × ZX.spider .Z 1 1 ⟨1, 4⟩)
-      ≃ZX ZX.spider .Z 1 1 ⟨1, 2⟩ := by
+    (ZX.spider .Z 1 1 (phaseLit 1 4) × ZX.spider .Z 1 1 (phaseLit 1 4))
+      ≃ZX ZX.spider .Z 1 1 (phaseLit 1 2) := by
   zx_alg_fusion 0 1
-  rfl
+  exact spider_eq_of_phase_eq (by unfold phaseLit; norm_num)
 
-theorem zxAlgFusion_parameterized (α β : Phase) :
+theorem zxAlgFusion_parameterized (α β : AlgPhase) :
     (ZX.spider .Z 1 1 α × ZX.spider .Z 1 1 β)
       ≃ZX ZX.spider .Z 1 1 (α + β) := by
   zx_alg_fusion 0 1
   rfl
 
-theorem zxAlgFusion_parameterized2 (α : Phase) :
-    (ZX.spider .Z 1 1 α × ZX.spider .Z 1 1 ⟨1, 2⟩)
-      ≃ZX ZX.spider .Z 1 1 (α + ⟨1, 2⟩) := by
+theorem zxAlgFusion_parameterized2 (α : AlgPhase) :
+    (ZX.spider .Z 1 1 α × ZX.spider .Z 1 1 (phaseLit 1 2))
+      ≃ZX ZX.spider .Z 1 1 (α + phaseLit 1 2) := by
   zx_alg_fusion 0 1
   rfl
 
-def zaf_pa3a (α : Phase) : ZX 1 1 := (ZX.spider .Z 1 1 α × ZX.spider .Z 1 1 (-α))
-theorem zxAlgFusion_parameterized3a (α : Phase) :
-    zaf_pa3a α
-      ≃ZX ZX.spider .Z 1 1 := by
+def zaf_pa3a (α : AlgPhase) : ZX 1 1 :=
+  ZX.spider .Z 1 1 α × ZX.spider .Z 1 1 (-α)
+theorem zxAlgFusion_parameterized3a (α : AlgPhase) :
+    zaf_pa3a α ≃ZX ZX.spider .Z 1 1 := by
   zx_alg_fusion 0 1
-  zx_alg_phase_cancel 0
-  rfl
+  exact spider_eq_of_phase_eq (by abel)
 
-def zaf_pa3b (α : Phase) : ZX 1 1 := zaf_pa3a α × ZX.spider .Z 1 1 ⟨1, 2⟩
-theorem zxAlgFusion_parameterized3b (α : Phase) :
+def zaf_pa3b (α : AlgPhase) : ZX 1 1 := zaf_pa3a α × ZX.spider .Z 1 1 (phaseLit 1 2)
+theorem zxAlgFusion_parameterized3b (α : AlgPhase) :
     zaf_pa3b α
-      ≃ZX (ZX.spider .Z 1 1 × ZX.spider .Z 1 1 ⟨1, 2⟩) := by
+      ≃ZX (ZX.spider .Z 1 1 × ZX.spider .Z 1 1 (phaseLit 1 2)) := by
   zx_alg_fusion 0 1
-  zx_alg_phase_cancel 0
-  rfl
+  refine ZX.compose_congr ?_ (ZX.equiv_refl _)
+  exact spider_eq_of_phase_eq (by abel)
+
+def zaf_pa3c (α : AlgPhase) : ZX 1 1 := zaf_pa3a α × ZX.spider .Z 1 1 α
+theorem zxAlgFusion_parameterized3c (α : AlgPhase) :
+    zaf_pa3c α ≃ZX ZX.spider .Z 1 1 α := by
+  zx_alg_fusion 0 1
+  zx_alg_fusion 0 1
+  exact spider_eq_of_phase_eq (by abel)
+
+/-! ### Headline win: full abelian-group reasoning on phases
+
+    With `AlgPhase = ℚ`, any polynomial phase identity reduces to ℚ algebra
+    and closes by `abel` / `ring` / `decide`. No `congr_phase`, no `decide`
+    on integer cross-multiplications. -/
+
+example (α : AlgPhase) :
+    ZX.spider .Z 1 1 (α + (-α) + α) ≃ZX ZX.spider .Z 1 1 α :=
+  spider_eq_of_phase_eq (by abel)
+
+example (α β : AlgPhase) :
+    ZX.spider .Z 1 1 (α + β + (-α)) ≃ZX ZX.spider .Z 1 1 β :=
+  spider_eq_of_phase_eq (by abel)
+
+example (α : AlgPhase) :
+    ZX.spider .Z 1 1 (2 * α - α) ≃ZX ZX.spider .Z 1 1 α :=
+  spider_eq_of_phase_eq (by ring)
 
 -- (2) Nested under a surrounding compose. IDs (DFS):
 --   s1=0 (outer left), s2=1 (target left), s3=2 (target right), s4=3 (outer right).
 theorem zxAlgFusion_nested :
     ZX.compose
-        (ZX.compose (ZX.spider .Z 1 1 ⟨1, 8⟩)
-          (ZX.compose (ZX.spider .Z 1 1 ⟨1, 4⟩) (ZX.spider .Z 1 1 ⟨1, 4⟩)))
-        (ZX.spider .Z 1 1 ⟨1, 8⟩)
+        (ZX.compose (ZX.spider .Z 1 1 (phaseLit 1 8))
+          (ZX.compose (ZX.spider .Z 1 1 (phaseLit 1 4))
+                      (ZX.spider .Z 1 1 (phaseLit 1 4))))
+        (ZX.spider .Z 1 1 (phaseLit 1 8))
       ≃ZX
     ZX.compose
-        (ZX.spider .Z 1 1 (⟨5, 8⟩))
-        (ZX.spider .Z 1 1 ⟨1, 8⟩) := by
+        (ZX.spider .Z 1 1 (phaseLit 5 8))
+        (ZX.spider .Z 1 1 (phaseLit 1 8)) := by
   zx_alg_fusion 1 2
   zx_alg_fusion 0 1
-  rfl
+  refine ZX.compose_congr ?_ (ZX.equiv_refl _)
+  exact spider_eq_of_phase_eq (by unfold phaseLit; norm_num)
 
 -- Axiom audit: proofs built by the tactic depend only on the standard
 -- three axioms — no project-local soundness axioms.

SpLean/Algebraic/ZX.lean
@@ -1,14 +1,17 @@
 import SpLean.ZXDiagram
+import SpLean.Algebraic.Phase
 
 namespace SpLean.Algebraic
 
 /-- A free-algebra ZX term, indexed by its arity (`n` inputs, `m` outputs).
-    Reuses `SpiderColor` and `Phase` from the graph-style `ZXDiagram` module. -/
+    Reuses `SpiderColor` from the graph-style `ZXDiagram` module; phases are
+    `AlgPhase = ℚ` (see `SpLean/Algebraic/Phase.lean`) so phase algebra
+    obeys `AddCommGroup` laws directly. -/
 inductive ZX : Nat → Nat → Type
   | empty    : ZX 0 0
   | wire     : ZX 1 1
   | hadamard : ZX 1 1
-  | spider   (c : SpiderColor) (n m : Nat) (φ : Phase := ⟨0, 1⟩) : ZX n m
+  | spider   (c : SpiderColor) (n m : Nat) (φ : AlgPhase := 0) : ZX n m
   | stack    {n m p q : Nat} : ZX n m → ZX p q → ZX (n + p) (m + q)
   | compose  {n m k : Nat} : ZX n m → ZX m k → ZX n k
 

Tests/PhaseLabel.lean
@@ -2,28 +2,26 @@ import LSpec
 import LeanSpider.Algebraic.Tactics
 
 /-! Unit tests for `LeanSpider.Algebraic.phaseExprToLabel` — the MetaM
-    walker that converts a `Phase`-typed `Expr` into its display string.
+    walker that converts an `AlgPhase`-typed `Expr` into its display string.
     Tests run at compile time via `#guard_msgs in #eval`; any divergence
     from the expected output fails the `lake build Tests` run.
 
-    Covers: concrete literals (gcd + mod-2π normalization via
-    `Phase.simplify`), surface `HAdd` / `Phase.add` combinators, free
-    variables, and mixed symbolic-plus-concrete expressions.
+    Covers: concrete `phaseLit` literals (gcd + mod-2π normalization happens
+    in the graph-side bridge `AlgPhase.toGraphPhase` + `Phase.simplify`),
+    surface `HAdd` / `Neg` combinators, free variables, and mixed
+    symbolic-plus-concrete expressions.
 
     These tests don't go through the LSpec runner because
     `phaseExprToLabel` is `MetaM`-bound and `LSpec.test` only accepts
     pure `Prop`s. They still gate the build, which is enough for CI. -/
 
-open Lean Elab Meta LeanSpider.Algebraic
+open Lean Elab Meta SpLean.Algebraic
 
-namespace LeanSpider.Tests.PhaseLabel
+namespace SpLean.Tests.PhaseLabel
 
-/-- Build a `Phase.mk n d` `Expr` (used as a literal building block). -/
-private def phaseLit (n : Int) (d : Nat) : MetaM Expr := do
-  let numE : Expr := Lean.toExpr n
-  let denE ← mkAppOptM ``OfNat.ofNat
-                #[some (mkConst ``PNat), some (Lean.toExpr d), none]
-  mkAppM ``Phase.mk #[numE, denE]
+/-- Build a `SpLean.Algebraic.phaseLit p q` `Expr` of type `AlgPhase`. -/
+private def phaseLit (p q : Int) : MetaM Expr := do
+  mkAppM ``SpLean.Algebraic.phaseLit #[Lean.toExpr p, Lean.toExpr q]
 
 -- == Concrete literals ==
 
@@ -43,74 +41,69 @@ private def phaseLit (n : Int) (d : Nat) : MetaM Expr := do
 #guard_msgs in #eval show MetaM String from do
   phaseExprToLabel (← phaseLit 2 3)
 
--- `Phase.simplify` gcd-reduces ⟨2, 4⟩ to ⟨1, 2⟩.
+-- ℚ auto-reduces 2/4 → 1/2; formatted as "π/2".
 /-- info: "π/2" -/
 #guard_msgs in #eval show MetaM String from do
   phaseExprToLabel (← phaseLit 2 4)
 
--- `Phase.simplify` mod-2π reduces ⟨5, 2⟩ → ⟨1, 2⟩ (since 5/2 ≡ 1/2 mod 2).
+-- `Phase.simplify` (in the graph-side display bridge) mod-2π reduces 5/2 → 1/2.
 /-- info: "π/2" -/
 #guard_msgs in #eval show MetaM String from do
   phaseExprToLabel (← phaseLit 5 2)
 
--- Euclidean mod in Lean: -1 % 8 = 7, so ⟨-1, 4⟩ formats as 7π/4
--- (the same angle on the circle, just expressed with non-negative
--- numerator). Documents the simplify-on-format behaviour.
+-- Lean's Euclidean mod gives -1 % 8 = 7, so the angle prints as 7π/4.
 /-- info: "7π/4" -/
 #guard_msgs in #eval show MetaM String from do
   phaseExprToLabel (← phaseLit (-1) 4)
 
 -- == Surface combinators on closed phases ==
---    Crucial: must NOT unfold to `Phase.mk (a*d + c*b) (b*d)` —
---    that was the bug from the previous iteration of this walker.
+--    Closed sums get evaluated as `ℚ` and rendered as a single literal:
+--    `phaseLit 1 4 + phaseLit 1 4` simplifies to `π/2`.
 
-/-- info: "π/4 + π/4" -/
+/-- info: "π/2" -/
 #guard_msgs in #eval show MetaM String from do
   let a ← phaseLit 1 4
   let b ← phaseLit 1 4
   phaseExprToLabel (← mkAppM ``HAdd.hAdd #[a, b])
 
--- Direct `Phase.add` (in case some caller bypasses the typeclass).
-/-- info: "π/4 + π/4" -/
-#guard_msgs in #eval show MetaM String from do
-  let a ← phaseLit 1 4
-  let b ← phaseLit 1 4
-  phaseExprToLabel (← mkAppM ``Phase.add #[a, b])
-
 -- == Symbolic free variables ==
 
 /-- info: "α" -/
 #guard_msgs in #eval show MetaM String from do
-  withLocalDeclD `α (mkConst ``Phase) fun α =>
+  withLocalDeclD `α (mkConst ``AlgPhase) fun α =>
     phaseExprToLabel α
 
 /-- info: "α + β" -/
 #guard_msgs in #eval show MetaM String from do
-  withLocalDeclD `α (mkConst ``Phase) fun α =>
-    withLocalDeclD `β (mkConst ``Phase) fun β => do
+  withLocalDeclD `α (mkConst ``AlgPhase) fun α =>
+    withLocalDeclD `β (mkConst ``AlgPhase) fun β => do
       phaseExprToLabel (← mkAppM ``HAdd.hAdd #[α, β])
 
 -- == Mixed: symbolic + concrete ==
---    The original bug case — these previously printed
---    `α + { num := 1, den := 2 }`.
 
 /-- info: "α + π/2" -/
 #guard_msgs in #eval show MetaM String from do
-  withLocalDeclD `α (mkConst ``Phase) fun α => do
+  withLocalDeclD `α (mkConst ``AlgPhase) fun α => do
     phaseExprToLabel (← mkAppM ``HAdd.hAdd #[α, ← phaseLit 1 2])
 
 /-- info: "π/2 + α" -/
 #guard_msgs in #eval show MetaM String from do
-  withLocalDeclD `α (mkConst ``Phase) fun α => do
+  withLocalDeclD `α (mkConst ``AlgPhase) fun α => do
     phaseExprToLabel (← mkAppM ``HAdd.hAdd #[← phaseLit 1 2, α])
 
 -- Nested combinators recurse correctly.
 /-- info: "α + β + π/2" -/
 #guard_msgs in #eval show MetaM String from do
-  withLocalDeclD `α (mkConst ``Phase) fun α =>
-    withLocalDeclD `β (mkConst ``Phase) fun β => do
+  withLocalDeclD `α (mkConst ``AlgPhase) fun α =>
+    withLocalDeclD `β (mkConst ``AlgPhase) fun β => do
       let inner ← mkAppM ``HAdd.hAdd #[α, β]
       phaseExprToLabel (← mkAppM ``HAdd.hAdd #[inner, ← phaseLit 1 2])
+
+-- Unary minus on a free variable.
+/-- info: "-α" -/
+#guard_msgs in #eval show MetaM String from do
+  withLocalDeclD `α (mkConst ``AlgPhase) fun α => do
+    phaseExprToLabel (← mkAppM ``Neg.neg #[α])
 
 -- A trivial `TestSeq` so that `Tests/All.lean`'s aggregate has a stable
 -- entry for this file. The real assertions are the `#guard_msgs` blocks