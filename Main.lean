adnathanail/LeanSpider

Make inputs/outputs horizontally align with their connected spiders, and align multi-qubit spiders nicely

Main.lean
@@ -148,18 +148,29 @@ example : ∃ d', exercise3point7 ≈z d' := by
 
 -- Algebraic-ZX terms can now be rendered too: `ZX.toHtml` converts them into
 -- the same `ZXDiagram` widget shown above.
-open LeanSpider.Algebraic in
+open LeanSpider.Algebraic
 def algSpider : ZX 1 1 := .spider .Z 1 1 ⟨1, 2⟩
 #html algSpider.toHtml
 
-open LeanSpider.Algebraic in
 def algFusionLHS : ZX 1 1 := .spider .Z 1 1 ⟨1, 4⟩ ⨾ .spider .Z 1 1 ⟨1, 4⟩
 #html algFusionLHS.toHtml
 
-open LeanSpider.Algebraic in
 def algStack : ZX 2 2 := .wire ⊗ZX .hadamard
 #html algStack.toHtml
 
-open LeanSpider.Algebraic in
 def algCnot : ZX 2 2 := (.spider .Z 1 2 ⟨0, 1⟩ ⊗ZX .wire) ⨾ (.wire ⊗ZX .spider .X 2 1 ⟨0, 1⟩)
 #html algCnot.toHtml
+
+def algLayoutTest1 : ZX 4 4 := algCnot ⊗ZX algCnot
+#html algLayoutTest1.toHtml
+
+def algLayoutTest2 : ZX 2 2 := algCnot ⨾ algCnot
+#html algLayoutTest2.toHtml
+
+def algLayoutTest3 : ZX 3 3 := (algCnot ⊗ZX .wire) ⨾ (.wire ⊗ZX algCnot)
+#html algLayoutTest3.toHtml
+
+def algLayoutTest4a : ZX 2 4 := (.spider .Z 1 3 ⟨0, 1⟩ ⊗ZX .wire)
+def algLayoutTest4b : ZX 4 2 := (.wire ⊗ZX .spider .Z 3 1 ⟨0, 1⟩)
+def algLayoutTest4 : ZX 2 2 := (.spider .Z 1 3 ⟨0, 1⟩ ⊗ZX .wire) ⨾ (.wire ⊗ZX .spider .X 3 1 ⟨0, 1⟩)
+#html algLayoutTest4.toHtml

adnathanail/splean

Main.lean
@@ -142,18 +142,29 @@ example : ∃ d', exercise3point7 ≈z d' := by
 
 -- Algebraic-ZX terms can now be rendered too: `ZX.toHtml` converts them into
 -- the same `ZXDiagram` widget shown above.
-open SpLean.Algebraic in
+open SpLean.Algebraic
 def algSpider : ZX 1 1 := .spider .Z 1 1 ⟨1, 2⟩
 #html algSpider.toHtml
 
-open SpLean.Algebraic in
 def algFusionLHS : ZX 1 1 := .spider .Z 1 1 ⟨1, 4⟩ ⨾ .spider .Z 1 1 ⟨1, 4⟩
 #html algFusionLHS.toHtml
 
-open SpLean.Algebraic in
 def algStack : ZX 2 2 := .wire ⊗ZX .hadamard
 #html algStack.toHtml
 
-open LeanSpider.Algebraic in
 def algCnot : ZX 2 2 := (.spider .Z 1 2 ⟨0, 1⟩ ⊗ZX .wire) ⨾ (.wire ⊗ZX .spider .X 2 1 ⟨0, 1⟩)
 #html algCnot.toHtml
+
+def algLayoutTest1 : ZX 4 4 := algCnot ⊗ZX algCnot
+#html algLayoutTest1.toHtml
+
+def algLayoutTest2 : ZX 2 2 := algCnot ⨾ algCnot
+#html algLayoutTest2.toHtml
+
+def algLayoutTest3 : ZX 3 3 := (algCnot ⊗ZX .wire) ⨾ (.wire ⊗ZX algCnot)
+#html algLayoutTest3.toHtml
+
+def algLayoutTest4a : ZX 2 4 := (.spider .Z 1 3 ⟨0, 1⟩ ⊗ZX .wire)
+def algLayoutTest4b : ZX 4 2 := (.wire ⊗ZX .spider .Z 3 1 ⟨0, 1⟩)
+def algLayoutTest4 : ZX 2 2 := (.spider .Z 1 3 ⟨0, 1⟩ ⊗ZX .wire) ⨾ (.wire ⊗ZX .spider .X 3 1 ⟨0, 1⟩)
+#html algLayoutTest4.toHtml