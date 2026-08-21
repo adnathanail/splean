adnathanail/LeanSpider

Build custom tactics for algebraic ZX diagrams

LeanSpider/Algebraic.lean
@@ -3,5 +3,7 @@ import LeanSpider.Algebraic.ZX
 import LeanSpider.Algebraic.Semantics
 import LeanSpider.Algebraic.Congruence
 import LeanSpider.Algebraic.SpiderFusion
+import LeanSpider.Algebraic.Rewrite
+import LeanSpider.Algebraic.Tactics
 import LeanSpider.Algebraic.Visualize
 import LeanSpider.Algebraic.Gates

LeanSpider/Algebraic/Rewrite.lean
@@ -1,0 +1,70 @@
+import LeanSpider.Algebraic.ZX
+import LeanSpider.ZXDiagram
+
+namespace LeanSpider.Algebraic
+
+open LeanSpider
+
+/-- Count of `ZXDiagram` nodes that `ZX n m` lowers to, excluding the
+    boundary inputs/outputs added at the top level. Mirrors `buildFrag`
+    in `Visualize.lean` exactly so the IDs the tactic computes match
+    those the user sees in the rendered diagram. -/
+def ZX.nodeCount : {n m : Nat} → ZX n m → Nat
+  | _, _, .empty    => 0
+  | _, _, .wire     => 1
+  | _, _, .hadamard => 1
+  | _, _, .spider _ _ _ _ => 1
+  | _, _, .stack   a b => a.nodeCount + b.nodeCount
+  | _, _, .compose a b => a.nodeCount + b.nodeCount
+
+/-- Attempt direct Z-spider fusion on a pair `(a, b)` that sit under a
+    single `compose`. Succeeds only when `a` is a Z-spider with output
+    arity 1 and `b` is a Z-spider with input arity 1; produces the fused
+    spider `spider .Z n k (α + β)` with the raw phase sum. -/
+def ZX.tryFuse {n mid k : Nat} (a : ZX n mid) (b : ZX mid k) :
+    Option (ZX n k) :=
+  match mid, a, b with
+  | 1, .spider .Z _ 1 α, .spider .Z 1 _ β => some (.spider .Z _ _ (α + β))
+  | _, _, _ => none
+
+/-- Walker for `applySpiderFusionAt`. Threads an offset counter through
+    the term in the same DFS order as `buildFrag` (so the IDs match the
+    rendered diagram). At each `compose` whose two children have offsets
+    `(idA, idB)`, attempts the direct fusion. -/
+def ZX.applySpiderFusionAtAux : {n m : Nat} → ZX n m → NodeId → NodeId → Nat →
+    Except String (ZX n m × Nat)
+  | _, _, .empty, _, _, off => .ok (.empty, off)
+  | _, _, .wire, _, _, off => .ok (.wire, off + 1)
+  | _, _, .hadamard, _, _, off => .ok (.hadamard, off + 1)
+  | _, _, .spider c i j φ, _, _, off => .ok (.spider c i j φ, off + 1)
+  | _, _, .stack a b, idA, idB, off => do
+      let (a', off1) ← ZX.applySpiderFusionAtAux a idA idB off
+      let (b', off2) ← ZX.applySpiderFusionAtAux b idA idB off1
+      .ok (.stack a' b', off2)
+  | _, _, .compose a b, idA, idB, off =>
+      let offA := off
+      let offB := off + a.nodeCount
+      let offEnd := offB + b.nodeCount
+      if offA = idA ∧ offB = idB then
+        match ZX.tryFuse a b with
+        | some fused => .ok (fused, offEnd)
+        | none =>
+            .error
+              s!"Nodes {idA} and {idB} are adjacent under a `compose`, \
+                 but the pair is not a fuseable Z-spider junction \
+                 (need Z-spider of arity (_, 1) followed by Z-spider \
+                 of arity (1, _))."
+      else do
+        let (a', off1) ← ZX.applySpiderFusionAtAux a idA idB offA
+        let (b', off2) ← ZX.applySpiderFusionAtAux b idA idB off1
+        .ok (.compose a' b', off2)
+
+/-- Top-level entry point: try to apply Z-spider fusion to the two
+    spiders identified by `idA` (output spider) and `idB` (input spider).
+    `idA`/`idB` are the IDs assigned by `ZX.toPositionedDiagram`. -/
+def ZX.applySpiderFusionAt {n m : Nat} (z : ZX n m) (idA idB : NodeId) :
+    Except String (ZX n m) := do
+  let (z', _) ← ZX.applySpiderFusionAtAux z idA idB 0
+  .ok z'
+
+end LeanSpider.Algebraic

LeanSpider/Algebraic/Tactics.lean
@@ -1,0 +1,110 @@
+import LeanSpider.Algebraic.SpiderFusion
+import LeanSpider.Algebraic.Congruence
+import LeanSpider.Algebraic.Rewrite
+
+open Lean Elab Tactic Meta
+
+namespace LeanSpider.Algebraic
+
+/-- Parse a goal of the form `lhs ≃ZX rhs` into its sides.
+    `ZX.equiv` has signature `{n m} → ZX n m → ZX n m → Prop` so the
+    elaborated expression is `@ZX.equiv n m lhs rhs` — 4 args. -/
+def parseAlgEquivGoal (goalType : Expr) : TacticM (Expr × Expr) := do
+  let goalType ← instantiateMVars goalType
+  let (fn, args) := (goalType.getAppFn, goalType.getAppArgs)
+  if fn.constName? == some ``ZX.equiv && args.size == 4 then
+    return (args[2]!, args[3]!)
+  throwError "Goal is not of the form `lhs ≃ZX rhs` (got: {goalType})"
+
+/-- Walk a `ZX n m` expression in the same DFS order as `buildFrag` in
+    `Visualize.lean`, building both the rewritten term and a proof
+    `original ≃ZX rewritten`. At the target `compose` (whose children
+    have node IDs `idA` and `idB`), applies `Z_spiderFusion`.
+
+    Returns `(rewritten, proof, endOffset)`. -/
+partial def buildFusionProof (z : Expr) (idA idB : Nat) (off : Nat) :
+    MetaM (Expr × Expr × Nat) := do
+  let z ← whnf z
+  let f := z.getAppFn
+  let name := f.constName?
+  match name with
+  | some ``ZX.empty =>
+      let proof ← mkAppM ``ZX.equiv_refl #[z]
+      return (z, proof, off)
+  | some ``ZX.wire =>
+      let proof ← mkAppM ``ZX.equiv_refl #[z]
+      return (z, proof, off + 1)
+  | some ``ZX.hadamard =>
+      let proof ← mkAppM ``ZX.equiv_refl #[z]
+      return (z, proof, off + 1)
+  | some ``ZX.spider =>
+      let proof ← mkAppM ``ZX.equiv_refl #[z]
+      return (z, proof, off + 1)
+  | some ``ZX.stack =>
+      -- @ZX.stack n m p q a b — args 4 and 5 are the children
+      let args := z.getAppArgs
+      let a := args[4]!
+      let b := args[5]!
+      let (a', proofA, off1) ← buildFusionProof a idA idB off
+      let (b', proofB, off2) ← buildFusionProof b idA idB off1
+      let stacked ← mkAppM ``ZX.stack #[a', b']
+      let proof ← mkAppM ``ZX.stack_congr #[proofA, proofB]
+      return (stacked, proof, off2)
+  | some ``ZX.compose =>
+      -- @ZX.compose n m k a b — args 3 and 4 are the children
+      let args := z.getAppArgs
+      let a := args[3]!
+      let b := args[4]!
+      let offA := off
+      -- Walk a first to determine offB
+      let (a', proofA, offB) ← buildFusionProof a idA idB offA
+      if offA == idA && offB == idB then
+        -- Target compose. Reach for Z_spiderFusion. Extract n, k, α, β
+        -- from the spiders a and b directly. Both are leaves (nodeCount 1),
+        -- so the end offset is just offB + 1.
+        let aWhnf ← whnf a
+        let bWhnf ← whnf b
+        let aArgs := aWhnf.getAppArgs   -- @ZX.spider .Z n 1 α
+        let bArgs := bWhnf.getAppArgs   -- @ZX.spider .Z 1 k β
+        unless aWhnf.getAppFn.constName? == some ``ZX.spider
+            && bWhnf.getAppFn.constName? == some ``ZX.spider
+            && aArgs.size == 4 && bArgs.size == 4 do
+          throwError "Nodes {idA} and {idB} are adjacent under a `compose`, \
+                     but the pair is not a fuseable Z-spider junction."
+        let n := aArgs[1]!
+        let α := aArgs[3]!
+        let k := bArgs[2]!
+        let β := bArgs[3]!
+        let proof ← mkAppM ``Z_spiderFusion #[n, k, α, β]
+        -- Construct the fused result: ZX.spider .Z n k (α + β)
+        let sumPhase ← mkAppM ``HAdd.hAdd #[α, β]
+        let colorZ := mkConst ``SpiderColor.Z
+        let fused ← mkAppM ``ZX.spider #[colorZ, n, k, sumPhase]
+        return (fused, proof, offB + 1)
+      else
+        let (b', proofB, offEnd) ← buildFusionProof b idA idB offB
+        let composed ← mkAppM ``ZX.compose #[a', b']
+        let proof ← mkAppM ``ZX.compose_congr #[proofA, proofB]
+        return (composed, proof, offEnd)
+  | _ => throwError "Unrecognized ZX expression head: {name}"
+
+/-- The main tactic engine — mirrors `applyRewrite` in
+    `LeanSpider/Tactics.lean` but for `≃ZX`. -/
+def applyZxAlgFusion (idA idB : Nat) : TacticM Unit := withMainContext do
+  let goal ← getMainGoal
+  let goalType ← goal.getType
+  let (lhs, rhs) ← parseAlgEquivGoal goalType
+  -- Build the rewrite proof
+  let (lhs', proof, _) ← buildFusionProof lhs idA idB 0
+  -- New residual goal: lhs' ≃ZX rhs
+  let newGoalType ← mkAppM ``ZX.equiv #[lhs', rhs]
+  let newGoal ← mkFreshExprMVar newGoalType
+  -- Combined proof: equiv_trans (lhs ≃ZX lhs') (lhs' ≃ZX rhs)
+  let trans ← mkAppM ``ZX.equiv_trans #[proof, newGoal]
+  goal.assign trans
+  setGoals [newGoal.mvarId!]
+
+elab "zx_alg_fusion " idA:num idB:num : tactic =>
+  applyZxAlgFusion idA.getNat idB.getNat
+
+end LeanSpider.Algebraic

adnathanail/splean

SpLean/Algebraic.lean
@@ -5,4 +5,7 @@ import SpLean.Algebraic.Visualize
 import SpLean.Algebraic.Semantics
 import SpLean.Algebraic.Congruence
 import SpLean.Algebraic.SpiderFusion
+import SpLean.Algebraic.Rewrite
+import SpLean.Algebraic.Tactics
+import SpLean.Algebraic.Visualize
 import SpLean.Algebraic.Gates

SpLean/Algebraic/Rewrite.lean
@@ -1,0 +1,70 @@
+import SpLean.Algebraic.ZX
+import SpLean.ZXDiagram
+
+namespace SpLean.Algebraic
+
+open SpLean
+
+/-- Count of `ZXDiagram` nodes that `ZX n m` lowers to, excluding the
+    boundary inputs/outputs added at the top level. Mirrors `buildFrag`
+    in `Visualize.lean` exactly so the IDs the tactic computes match
+    those the user sees in the rendered diagram. -/
+def ZX.nodeCount : {n m : Nat} → ZX n m → Nat
+  | _, _, .empty    => 0
+  | _, _, .wire     => 1
+  | _, _, .hadamard => 1
+  | _, _, .spider _ _ _ _ => 1
+  | _, _, .stack   a b => a.nodeCount + b.nodeCount
+  | _, _, .compose a b => a.nodeCount + b.nodeCount
+
+/-- Attempt direct Z-spider fusion on a pair `(a, b)` that sit under a
+    single `compose`. Succeeds only when `a` is a Z-spider with output
+    arity 1 and `b` is a Z-spider with input arity 1; produces the fused
+    spider `spider .Z n k (α + β)` with the raw phase sum. -/
+def ZX.tryFuse {n mid k : Nat} (a : ZX n mid) (b : ZX mid k) :
+    Option (ZX n k) :=
+  match mid, a, b with
+  | 1, .spider .Z _ 1 α, .spider .Z 1 _ β => some (.spider .Z _ _ (α + β))
+  | _, _, _ => none
+
+/-- Walker for `applySpiderFusionAt`. Threads an offset counter through
+    the term in the same DFS order as `buildFrag` (so the IDs match the
+    rendered diagram). At each `compose` whose two children have offsets
+    `(idA, idB)`, attempts the direct fusion. -/
+def ZX.applySpiderFusionAtAux : {n m : Nat} → ZX n m → NodeId → NodeId → Nat →
+    Except String (ZX n m × Nat)
+  | _, _, .empty, _, _, off => .ok (.empty, off)
+  | _, _, .wire, _, _, off => .ok (.wire, off + 1)
+  | _, _, .hadamard, _, _, off => .ok (.hadamard, off + 1)
+  | _, _, .spider c i j φ, _, _, off => .ok (.spider c i j φ, off + 1)
+  | _, _, .stack a b, idA, idB, off => do
+      let (a', off1) ← ZX.applySpiderFusionAtAux a idA idB off
+      let (b', off2) ← ZX.applySpiderFusionAtAux b idA idB off1
+      .ok (.stack a' b', off2)
+  | _, _, .compose a b, idA, idB, off =>
+      let offA := off
+      let offB := off + a.nodeCount
+      let offEnd := offB + b.nodeCount
+      if offA = idA ∧ offB = idB then
+        match ZX.tryFuse a b with
+        | some fused => .ok (fused, offEnd)
+        | none =>
+            .error
+              s!"Nodes {idA} and {idB} are adjacent under a `compose`, \
+                 but the pair is not a fuseable Z-spider junction \
+                 (need Z-spider of arity (_, 1) followed by Z-spider \
+                 of arity (1, _))."
+      else do
+        let (a', off1) ← ZX.applySpiderFusionAtAux a idA idB offA
+        let (b', off2) ← ZX.applySpiderFusionAtAux b idA idB off1
+        .ok (.compose a' b', off2)
+
+/-- Top-level entry point: try to apply Z-spider fusion to the two
+    spiders identified by `idA` (output spider) and `idB` (input spider).
+    `idA`/`idB` are the IDs assigned by `ZX.toPositionedDiagram`. -/
+def ZX.applySpiderFusionAt {n m : Nat} (z : ZX n m) (idA idB : NodeId) :
+    Except String (ZX n m) := do
+  let (z', _) ← ZX.applySpiderFusionAtAux z idA idB 0
+  .ok z'
+
+end SpLean.Algebraic

SpLean/Algebraic/Tactics.lean
@@ -1,0 +1,110 @@
+import SpLean.Algebraic.SpiderFusion
+import SpLean.Algebraic.Congruence
+import SpLean.Algebraic.Rewrite
+
+open Lean Elab Tactic Meta
+
+namespace SpLean.Algebraic
+
+/-- Parse a goal of the form `lhs ≃ZX rhs` into its sides.
+    `ZX.equiv` has signature `{n m} → ZX n m → ZX n m → Prop` so the
+    elaborated expression is `@ZX.equiv n m lhs rhs` — 4 args. -/
+def parseAlgEquivGoal (goalType : Expr) : TacticM (Expr × Expr) := do
+  let goalType ← instantiateMVars goalType
+  let (fn, args) := (goalType.getAppFn, goalType.getAppArgs)
+  if fn.constName? == some ``ZX.equiv && args.size == 4 then
+    return (args[2]!, args[3]!)
+  throwError "Goal is not of the form `lhs ≃ZX rhs` (got: {goalType})"
+
+/-- Walk a `ZX n m` expression in the same DFS order as `buildFrag` in
+    `Visualize.lean`, building both the rewritten term and a proof
+    `original ≃ZX rewritten`. At the target `compose` (whose children
+    have node IDs `idA` and `idB`), applies `Z_spiderFusion`.
+
+    Returns `(rewritten, proof, endOffset)`. -/
+partial def buildFusionProof (z : Expr) (idA idB : Nat) (off : Nat) :
+    MetaM (Expr × Expr × Nat) := do
+  let z ← whnf z
+  let f := z.getAppFn
+  let name := f.constName?
+  match name with
+  | some ``ZX.empty =>
+      let proof ← mkAppM ``ZX.equiv_refl #[z]
+      return (z, proof, off)
+  | some ``ZX.wire =>
+      let proof ← mkAppM ``ZX.equiv_refl #[z]
+      return (z, proof, off + 1)
+  | some ``ZX.hadamard =>
+      let proof ← mkAppM ``ZX.equiv_refl #[z]
+      return (z, proof, off + 1)
+  | some ``ZX.spider =>
+      let proof ← mkAppM ``ZX.equiv_refl #[z]
+      return (z, proof, off + 1)
+  | some ``ZX.stack =>
+      -- @ZX.stack n m p q a b — args 4 and 5 are the children
+      let args := z.getAppArgs
+      let a := args[4]!
+      let b := args[5]!
+      let (a', proofA, off1) ← buildFusionProof a idA idB off
+      let (b', proofB, off2) ← buildFusionProof b idA idB off1
+      let stacked ← mkAppM ``ZX.stack #[a', b']
+      let proof ← mkAppM ``ZX.stack_congr #[proofA, proofB]
+      return (stacked, proof, off2)
+  | some ``ZX.compose =>
+      -- @ZX.compose n m k a b — args 3 and 4 are the children
+      let args := z.getAppArgs
+      let a := args[3]!
+      let b := args[4]!
+      let offA := off
+      -- Walk a first to determine offB
+      let (a', proofA, offB) ← buildFusionProof a idA idB offA
+      if offA == idA && offB == idB then
+        -- Target compose. Reach for Z_spiderFusion. Extract n, k, α, β
+        -- from the spiders a and b directly. Both are leaves (nodeCount 1),
+        -- so the end offset is just offB + 1.
+        let aWhnf ← whnf a
+        let bWhnf ← whnf b
+        let aArgs := aWhnf.getAppArgs   -- @ZX.spider .Z n 1 α
+        let bArgs := bWhnf.getAppArgs   -- @ZX.spider .Z 1 k β
+        unless aWhnf.getAppFn.constName? == some ``ZX.spider
+            && bWhnf.getAppFn.constName? == some ``ZX.spider
+            && aArgs.size == 4 && bArgs.size == 4 do
+          throwError "Nodes {idA} and {idB} are adjacent under a `compose`, \
+                     but the pair is not a fuseable Z-spider junction."
+        let n := aArgs[1]!
+        let α := aArgs[3]!
+        let k := bArgs[2]!
+        let β := bArgs[3]!
+        let proof ← mkAppM ``Z_spiderFusion #[n, k, α, β]
+        -- Construct the fused result: ZX.spider .Z n k (α + β)
+        let sumPhase ← mkAppM ``HAdd.hAdd #[α, β]
+        let colorZ := mkConst ``SpiderColor.Z
+        let fused ← mkAppM ``ZX.spider #[colorZ, n, k, sumPhase]
+        return (fused, proof, offB + 1)
+      else
+        let (b', proofB, offEnd) ← buildFusionProof b idA idB offB
+        let composed ← mkAppM ``ZX.compose #[a', b']
+        let proof ← mkAppM ``ZX.compose_congr #[proofA, proofB]
+        return (composed, proof, offEnd)
+  | _ => throwError "Unrecognized ZX expression head: {name}"
+
+/-- The main tactic engine — mirrors `applyRewrite` in
+    `SpLean/Tactics.lean` but for `≃ZX`. -/
+def applyZxAlgFusion (idA idB : Nat) : TacticM Unit := withMainContext do
+  let goal ← getMainGoal
+  let goalType ← goal.getType
+  let (lhs, rhs) ← parseAlgEquivGoal goalType
+  -- Build the rewrite proof
+  let (lhs', proof, _) ← buildFusionProof lhs idA idB 0
+  -- New residual goal: lhs' ≃ZX rhs
+  let newGoalType ← mkAppM ``ZX.equiv #[lhs', rhs]
+  let newGoal ← mkFreshExprMVar newGoalType
+  -- Combined proof: equiv_trans (lhs ≃ZX lhs') (lhs' ≃ZX rhs)
+  let trans ← mkAppM ``ZX.equiv_trans #[proof, newGoal]
+  goal.assign trans
+  setGoals [newGoal.mvarId!]
+
+elab "zx_alg_fusion " idA:num idB:num : tactic =>
+  applyZxAlgFusion idA.getNat idB.getNat
+
+end SpLean.Algebraic