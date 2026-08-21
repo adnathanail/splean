adnathanail/LeanSpider

Extract out common gates

LeanSpider/Algebraic.lean
@@ -3,3 +3,4 @@ import LeanSpider.Algebraic.ZX
 import LeanSpider.Algebraic.Semantics
 import LeanSpider.Algebraic.SpiderFusion
 import LeanSpider.Algebraic.Visualize
+import LeanSpider.Algebraic.Gates

LeanSpider/Algebraic/Gates.lean
@@ -1,0 +1,18 @@
+import LeanSpider.Algebraic.ZX
+import LeanSpider.Algebraic.Visualize
+
+open LeanSpider.Algebraic
+
+def GateCNOT : ZX 2 2 := (.spider .Z 1 2 ⊗ .wire) × (.wire ⊗ .spider .X 2 1)
+#html GateCNOT.toHtml
+
+def GateNOTC : ZX 2 2 := (.spider .X 1 2 ⊗ .wire) × (.wire ⊗ .spider .Z 2 1)
+#html GateNOTC.toHtml
+
+def GateCX : ZX 2 2 :=
+  (
+    (.spider .Z 1 2 ⊗ .wire) ×
+    (.wire ⊗ .hadamard ⊗ .wire)
+  ) ×
+  (.wire ⊗ .spider .Z 2 1)
+#html GateCX.toHtml

adnathanail/splean

SpLean/Algebraic.lean
@@ -4,3 +4,4 @@ import SpLean.Algebraic.Render
 import SpLean.Algebraic.Visualize
 import SpLean.Algebraic.Semantics
 import SpLean.Algebraic.SpiderFusion
+import SpLean.Algebraic.Gates

SpLean/Algebraic/Gates.lean
@@ -1,0 +1,18 @@
+import SpLean.Algebraic.ZX
+import SpLean.Algebraic.Visualize
+
+open SpLean.Algebraic
+
+def GateCNOT : ZX 2 2 := (.spider .Z 1 2 ⊗ .wire) × (.wire ⊗ .spider .X 2 1)
+#html GateCNOT.toHtml
+
+def GateNOTC : ZX 2 2 := (.spider .X 1 2 ⊗ .wire) × (.wire ⊗ .spider .Z 2 1)
+#html GateNOTC.toHtml
+
+def GateCX : ZX 2 2 :=
+  (
+    (.spider .Z 1 2 ⊗ .wire) ×
+    (.wire ⊗ .hadamard ⊗ .wire)
+  ) ×
+  (.wire ⊗ .spider .Z 2 1)
+#html GateCX.toHtml