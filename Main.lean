adnathanail/LeanSpider

Represent CNOT algebraically

Main.lean
@@ -160,3 +160,6 @@ open LeanSpider.Algebraic in
 def algStack : ZX 2 2 := .wire ⊗ZX .hadamard
 #html algStack.toHtml
 
+open LeanSpider.Algebraic in
+def algCnot : ZX 2 2 := (.spider .Z 1 2 ⟨0, 1⟩ ⊗ZX .wire) ⨾ (.wire ⊗ZX .spider .X 2 1 ⟨0, 1⟩)
+#html algCnot.toHtml

adnathanail/splean

Main.lean
@@ -153,3 +153,7 @@ def algFusionLHS : ZX 1 1 := .spider .Z 1 1 ⟨1, 4⟩ ⨾ .spider .Z 1 1 ⟨1, 
 open SpLean.Algebraic in
 def algStack : ZX 2 2 := .wire ⊗ZX .hadamard
 #html algStack.toHtml
+
+open SpLean.Algebraic in
+def algCnot : ZX 2 2 := (.spider .Z 1 2 ⟨0, 1⟩ ⊗ZX .wire) ⨾ (.wire ⊗ZX .spider .X 2 1 ⟨0, 1⟩)
+#html algCnot.toHtml