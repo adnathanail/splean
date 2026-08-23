f7003175d495a7a4d52a508c6b02895c18d17848
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-08-23 12:43:13 +0200

    Extract out common boundary assignments

--- SpLean/Algebraic/Testing.lean
+++ SpLean/Algebraic/Testing.lean
@@ -63,6 +63,10 @@ theorem scalar_sem_alpha (α : Phase) (f g : Wires 0) :
   X-basis states (pqs eq 3.3)
 -/
 
+-- Boundary assignments of a single wire
+abbrev zeroAmpl : Wires 1 := fun _ => false
+abbrev oneAmpl : Wires 1 := fun _ => true
+
 -- TODO track scalar factors
 -- (Z0)- = √2|+⟩ = |0⟩ + |1⟩
 --   no input wires so `f : Wires 0`
@@ -71,11 +75,11 @@ theorem scalar_sem_alpha (α : Phase) (f g : Wires 0) :
 abbrev plusState : ZX 0 1 := .spider .Z 0 1 ⟨0, 1⟩
 #zx plusState
 theorem z_sem_plus_state_zero_ampl (f : Wires 0) :
-    plusState.sem f (fun _ => false) = 1 := by
+    plusState.sem f zeroAmpl = 1 := by
   rw [ZX.sem, zSpiderSem, Phase.angle]
   norm_num
 theorem z_sem_plus_state_one_ampl (f : Wires 0) :
-    plusState.sem f (fun _ => true) = 1 := by
+    plusState.sem f oneAmpl = 1 := by
   rw [ZX.sem, zSpiderSem, Phase.angle]
   norm_num
 -- We can prove both with one theorem by splitting on the output wire indicator
@@ -94,10 +98,10 @@ theorem z_sem_plus_state (f : Wires 0) (g : Wires 1) :
 abbrev minusState : ZX 0 1 := .spider .Z 0 1 ⟨1, 1⟩
 #zx minusState
 theorem z_sem_minus_state_zero_ampl (f : Wires 0) :
-    minusState.sem f (fun _ => false) = 1 := by
+    minusState.sem f zeroAmpl = 1 := by
   rw [ZX.sem, zSpiderSem, Phase.angle]
   norm_num
 theorem z_sem_minus_state_one_ampla (f : Wires 0) :
-    minusState.sem f (fun _ => true) = -1 := by
+    minusState.sem f oneAmpl = -1 := by
   rw [ZX.sem, zSpiderSem, Phase.angle]
   norm_num [Complex.exp_pi_mul_I]
