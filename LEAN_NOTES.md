3914435e58cb96e96f7c6106113d84894f0b6703
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-08-23 12:06:16 +0200

    Neaten Z-spider scalar tests and add stubs for X-spider scalar tests

--- LEAN_NOTES.md
+++ LEAN_NOTES.md
@@ -1,4 +1,26 @@
+## Definitions and abbreviations
+```lean
+-- Definitions must be expanded in proofs
+def greenCircle : ZX 0 0 := .spider .Z 0 0 ⟨0, 1⟩
+theorem something := by
+  rw [greenCircle, nextStep]
+
+-- Abbreviations are automatic
+abbrev greenCircle : ZX 0 0 := .spider .Z 0 0 ⟨0, 1⟩
+theorem something := by
+  rw [nextStep]
+
+-- Parametric defs/abbrevs
+def greenAlphaCircle (α : Phase) : ZX 0 0 := .spider .Z 0 0 α
+abbrev greenAlphaCircle (α : Phase) : ZX 0 0 := .spider .Z 0 0 α
+```
+
+## Tactics
+
 `field_simp`
 https://leanprover-community.github.io/mathlib4_docs/Mathlib/Tactic/FieldSimp.html
 Tactic to clear denominators in algebraic expressions.
 
+`norm_num`
+Tactic to 'normalise' (simplify) numerical expressions
+https://leanprover-community.github.io/mathlib4_docs/Mathlib/Tactic/NormNum/Core.html
--- SpLean/Algebraic/Testing.lean
+++ SpLean/Algebraic/Testing.lean
@@ -3,30 +3,59 @@ import SpLean.Panel
 
 open SpLean.Algebraic
 
+noncomputable abbrev eiTheta (θ : ℝ) : ℂ := Complex.exp (θ * Complex.I)
+noncomputable abbrev rootTwo : ℂ := Real.sqrt 2
+
 -- Empty diagram = 1
-def emptyDiagram : ZX 0 0 := .empty
+abbrev emptyDiagram : ZX 0 0 := .empty
 #zx emptyDiagram
 theorem empty_sem (f g : Wires 0) : emptyDiagram.sem f g = 1 := by
-  rw [emptyDiagram, ZX.sem]
+  rw [ZX.sem]
 
-/-- Zero-arity phaseless Z spider = 2 -/
-def greenCircle : ZX 0 0 := .spider .Z 0 0 ⟨0, 1⟩
+/--
+  Scalars (pqs 3.1.4)
+--/
+
+-- Zero-arity phaseless Z spider = 2
+abbrev greenCircle : ZX 0 0 := .spider .Z 0 0 ⟨0, 1⟩
 #zx greenCircle
 theorem scalar_sem_two (f g : Wires 0) : greenCircle.sem f g = 2 := by
-  rw [greenCircle, ZX.sem, zSpiderSem, Phase.angle]
+  rw [ZX.sem, zSpiderSem, Phase.angle]
   norm_num
 
-/-- Zero-arity π-phase Z-spider = 0 -/
-def greenPiCircle : ZX 0 0 := .spider .Z 0 0 ⟨1, 1⟩
+-- Zero-arity π-phase Z-spider = 0
+abbrev greenPiCircle : ZX 0 0 := .spider .Z 0 0 ⟨1, 1⟩
 #zx greenPiCircle
 theorem scalar_sem_pi (f g : Wires 0) : greenPiCircle.sem f g = 0 := by
-  rw [greenPiCircle, ZX.sem, zSpiderSem, Phase.angle]
+  rw [ZX.sem, zSpiderSem, Phase.angle]
   norm_num
 
-/-- Zero-arity α-phase Z-spider = 1 + e^{iα} -/
-def greenAlphaCircle (α : Phase) : ZX 0 0 := .spider .Z 0 0 α
+-- Zero-arity α-phase Z-spider = 1 + e^{iα}
+abbrev greenAlphaCircle (α : Phase) : ZX 0 0 := .spider .Z 0 0 α
 #zx greenAlphaCircle ⟨1, 4⟩  -- TODO display parametric phases
 theorem scalar_sem_alpha (α : Phase) (f g : Wires 0) :
-    (greenAlphaCircle α).sem f g = 1 + Complex.exp (α.angle * Complex.I) := by
-  rw [greenAlphaCircle, ZX.sem, zSpiderSem]
+    (greenAlphaCircle α).sem f g = 1 + eiTheta α.angle := by
+  rw [ZX.sem, zSpiderSem]
   norm_num
+
+-- -- Phaseless X spider linked to α-phase Z spider = √2
+-- abbrev redCircleAnyGreen (α : Phase) : ZX 0 0 := (.spider .X 0 1 ⟨0, 1⟩) × (.spider .Z 1 0 α)
+-- #zx redCircleAnyGreen ⟨1, 4⟩  -- TODO display parametric phases
+-- theorem scalar_sem_sqrt_two (α : Phase) (f g : Wires 0) :
+--     (redCircleAnyGreen α).sem f g = rootTwo := by
+--   rw [ZX.sem]
+
+-- -- π-phase X spider linked to α-phase Z spider = √2 e^{iα}
+-- abbrev redPiCircleAnyGreen (α : Phase) : ZX 0 0 := (.spider .X 0 1 ⟨1, 1⟩) × (.spider .Z 1 0 α)
+-- #zx redPiCircleAnyGreen ⟨1, 4⟩  -- TODO display parametric phases
+-- theorem scalar_sem_sqrt_two_e_i_alpha (α : Phase) (f g : Wires 0) :
+--     (redPiCircleAnyGreen α).sem f g = rootTwo * eiTheta α.angle := by
+--   rw [ZX.sem]
+
+-- -- Phaseles X spider triple-linked to phaseless Z spider = 1/√2
+-- abbrev redCircleTripleLinkGreenCircle : ZX 0 0 := (.spider .X 0 3 ⟨0, 1⟩) × (.spider .Z 3 0 ⟨0, 1⟩)
+-- #zx redCircleTripleLinkGreenCircle
+-- theorem scalar_sem_one_over_sqrt_two (f g : Wires 0) :
+--     redCircleTripleLinkGreenCircle.sem f g = 1 / rootTwo := by
+--   rw [ZX.sem]
+
