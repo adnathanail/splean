fd01558a8dd16ff3d78e5c2f625dfcdc349b0ad5
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-08-25 17:50:39 +0200

    Compose two Z gates

--- SemanticsTesting/06Compose.lean
+++ SemanticsTesting/06Compose.lean
@@ -2,10 +2,11 @@ import SemanticsTesting.Utils
 
 open SpLean.Algebraic
 
-abbrev twoWires : ZX 1 1 := .wire × .wire
+abbrev identityMatrix : Matrix (Fin 2) (Fin 2) ℂ := !![1, 0; 0, 1]
+
+abbrev twoWires : ZX 1 1 := .wire ≫ .wire
 #zx twoWires
-theorem two_wire_sem :
-    twoWires.sem = (!![1, 0; 0, 1] : Matrix (Fin 2) (Fin 2) ℂ) := by
+theorem two_wire_sem : twoWires.sem = identityMatrix := by
   apply funext; intro f
   apply funext; intro g
   rw [ZX.sem, wiresMat2]
@@ -13,3 +14,14 @@ theorem two_wire_sem :
   norm_num
   field_simp
   cases f 0 <;> cases g 0 <;> norm_num <;> decide
+
+abbrev twoZGates : ZX 1 1 := (.spider .Z 1 1 ⟨1, 1⟩) ≫ (.spider .Z 1 1 ⟨1, 1⟩)
+#zx twoZGates
+theorem two_z_gates_sem : twoZGates.sem = identityMatrix := by
+  apply funext; intro f
+  apply funext; intro g
+  rw [ZX.sem, wiresMat2]
+  rw [show (Finset.univ : Finset (Wires 1)) = {zeroAmpl, oneAmpl} from by decide]
+  rw [Finset.sum_pair (by decide)]
+  simp [ZX.sem, zSpiderSem, Phase.angle]
+  cases f 0 <;> cases g 0 <;> simp
