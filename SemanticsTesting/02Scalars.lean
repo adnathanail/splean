e9db4e3e7f781ab17f58a66a8c9df67a5cfb9765
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-08-28 00:05:02 +0200

    AlgPhase: 02Scalars mechanical replacement

--- SemanticsTesting/02Scalars.lean
+++ SemanticsTesting/02Scalars.lean
@@ -7,45 +7,45 @@ open SpLean.Algebraic
 --/
 
 -- Zero-arity phaseless Z spider = 2
-abbrev greenCircle : ZX 0 0 := .spider .Z 0 0 ⟨0, 1⟩
+abbrev greenCircle : ZX 0 0 := .spider .Z 0 0 0
 #zx greenCircle
 theorem scalar_sem_two (f g : Wires 0) : greenCircle.sem f g = 2 := by
-  rw [ZX.sem, zSpiderSem, Phase.angle]
+  rw [ZX.sem, zSpiderSem, AlgPhase.angle, AlgPhase.toRat]
   norm_num
 
 -- Zero-arity π-phase Z-spider = 0
-abbrev greenPiCircle : ZX 0 0 := .spider .Z 0 0 ⟨1, 1⟩
+abbrev greenPiCircle : ZX 0 0 := .spider .Z 0 0 1
 #zx greenPiCircle
 theorem scalar_sem_pi (f g : Wires 0) : greenPiCircle.sem f g = 0 := by
-  rw [ZX.sem, zSpiderSem, Phase.angle]
+  rw [ZX.sem, zSpiderSem, AlgPhase.angle, AlgPhase.toRat]
   norm_num
 
 -- Zero-arity α-phase Z-spider = 1 + e^{iα}
-abbrev greenAlphaCircle (α : Phase) : ZX 0 0 := .spider .Z 0 0 α
-#zx greenAlphaCircle ⟨1, 4⟩  -- TODO display parametric phases
-theorem scalar_sem_alpha (α : Phase) (f g : Wires 0) :
+abbrev greenAlphaCircle (α : AlgPhase) : ZX 0 0 := .spider .Z 0 0 α
+#zx greenAlphaCircle (1 / 4)  -- TODO display parametric phases
+theorem scalar_sem_alpha (α : AlgPhase) (f g : Wires 0) :
     (greenAlphaCircle α).sem f g = 1 + eiTheta α.angle := by
   rw [ZX.sem, zSpiderSem]
   norm_num
 
 -- Phaseless X spider linked to α-phase Z spider = √2
-abbrev redCircleAnyGreen (α : Phase) : ZX 0 0 := (.spider .X 0 1 ⟨0, 1⟩) ≫ (.spider .Z 1 0 α)
-#zx redCircleAnyGreen ⟨1, 4⟩
-theorem scalar_sem_sqrt_two (α : Phase) (f g : Wires 0) :
+abbrev redCircleAnyGreen (α : AlgPhase) : ZX 0 0 := (.spider .X 0 1 0) ≫ (.spider .Z 1 0 α)
+#zx redCircleAnyGreen (1 / 4)
+theorem scalar_sem_sqrt_two (α : AlgPhase) (f g : Wires 0) :
     (redCircleAnyGreen α).sem f g = rootTwo := by
   rw [ZX.sem]
-  simp only [sum_wires1, ZX.sem, xSpiderSem, zSpiderSem, hadSem, Phase.angle]
+  simp only [sum_wires1, ZX.sem, xSpiderSem, zSpiderSem, hadSem, AlgPhase.angle, AlgPhase.toRat]
   norm_num
   rw [two_times_one_over_root_two_eq_root_two_complex]
 
 -- π-phase X spider linked to α-phase Z spider = √2 e^{iα}
-abbrev redPiCircleAnyGreen (α : Phase) : ZX 0 0 := (.spider .X 0 1 ⟨1, 1⟩) ≫ (.spider .Z 1 0 α)
-#zx redPiCircleAnyGreen ⟨1, 4⟩
-theorem scalar_sem_sqrt_two_e_i_alpha (α : Phase) (f g : Wires 0) :
+abbrev redPiCircleAnyGreen (α : AlgPhase) : ZX 0 0 := (.spider .X 0 1 1) ≫ (.spider .Z 1 0 α)
+#zx redPiCircleAnyGreen (1 / 4)
+theorem scalar_sem_sqrt_two_e_i_alpha (α : AlgPhase) (f g : Wires 0) :
     (redPiCircleAnyGreen α).sem f g = rootTwo * eiTheta α.angle := by
   unfold rootTwo eiTheta
   rw [ZX.sem]
-  simp only [sum_wires1, ZX.sem, xSpiderSem, zSpiderSem, hadSem, Phase.angle]
+  simp only [sum_wires1, ZX.sem, xSpiderSem, zSpiderSem, hadSem, AlgPhase.angle, AlgPhase.toRat]
   norm_num
   rw [two_times_one_over_root_two_eq_root_two_complex]
 
@@ -58,12 +58,12 @@ lemma sum_bool_endpoints {n : ℕ} {M : Type*} [AddCommMonoid M] (a b : M) :
   norm_num
 
 -- Phaseles X spider triple-linked to phaseless Z spider = 1/√2
-abbrev redCircleTripleLinkGreenCircle : ZX 0 0 := (.spider .X 0 3 ⟨0, 1⟩) ≫ (.spider .Z 3 0 ⟨0, 1⟩)
+abbrev redCircleTripleLinkGreenCircle : ZX 0 0 := (.spider .X 0 3 0) ≫ (.spider .Z 3 0 0)
 #zx redCircleTripleLinkGreenCircle
 theorem scalar_sem_one_over_sqrt_two (f g : Wires 0) :
     redCircleTripleLinkGreenCircle.sem f g = 1 / rootTwo := by
   -- Simp definitions
-  simp only [ZX.sem, xSpiderSem, zSpiderSem, hadSem, Phase.angle]
+  simp only [ZX.sem, xSpiderSem, zSpiderSem, hadSem, AlgPhase.angle, AlgPhase.toRat]
   -- Remove product over Fin 0
   simp only [Finset.univ_eq_empty, Finset.prod_empty, one_mul]
   -- Remove conditions over Fin 0
