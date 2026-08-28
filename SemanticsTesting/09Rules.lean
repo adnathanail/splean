232856fc51a6bab9e54d13bfb52262e3040aedae
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-08-28 21:31:39 +0200

    Test X gate fusion

--- SemanticsTesting/09Rules.lean
+++ SemanticsTesting/09Rules.lean
@@ -9,3 +9,7 @@ theorem two_t_gates_equiv_s_gate :
 theorem three_t_gates_fusion :
     Gate.T ≫ Gate.T ≫ Gate.T ≈zx ZX.spider .Z 1 1 (3π/4) := by
   zx_rw [zSpider_fusion, zSpider_fusion]
+
+theorem two_x_gates_fusion :
+    Gate.X ≫ Gate.X ≈zx ZX.spider .X 1 1 (2π) := by
+  zx_rw [xSpider_fusion]
