d2a227df328eef5eaf71c2a4d2d5c737a3c2acd9
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-08-28 14:05:26 +0200

    Test AlgPhase format

---
+++ Tests/Algebraic/AlgPhase.lean
@@ -0,0 +1,27 @@
+import LSpec
+import SpLean.Algebraic.AlgPhase
+
+namespace Tests.Algebraic.AlgPhase
+
+open LSpec
+
+private def pi : _root_.AlgPhase := 1
+private def twoPi : _root_.AlgPhase := 2
+private def piOverTwo : _root_.AlgPhase := (1 / 2)
+private def minusPi : _root_.AlgPhase := -1
+private def minusPiOverTwo : _root_.AlgPhase := -(1 / 2)
+private def threePi : _root_.AlgPhase := 3
+
+private def formatTests : TestSeq :=
+  group "format" $
+    test "pi" (pi.format = "π") $
+    test "two pi" (twoPi.format = "2π") $
+    test "pi over two" (piOverTwo.format = "π/2") $
+    test "minus pi" (minusPi.format = "-π") $
+    test "minus pi over two" (minusPiOverTwo.format = "-π/2")
+
+def tests : TestSeq :=
+  group "AlgPhase" $
+    formatTests
+
+end Tests.Algebraic.AlgPhase
