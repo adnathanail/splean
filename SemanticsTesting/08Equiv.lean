d519692fc4a68246863b6e66f497871ce3fecabe
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-08-28 18:45:24 +0200

    Add equivalence operator ≈zx

--- SemanticsTesting/08Equiv.lean
+++ SemanticsTesting/08Equiv.lean
@@ -4,7 +4,19 @@ open SpLean.Algebraic
 
 abbrev plusState : ZX 0 1 := .spider .Z 0 1
 theorem plusStateIsEquivalentToItself :
-    ZX.Equiv plusState plusState := by
+    plusState ≈zx plusState := by
   rw [ZX.Equiv]
   use 1
   norm_num
+
+abbrev plusStateTwoPi : ZX 0 1 := .spider .Z 0 1 2
+#zx plusStateTwoPi
+theorem plusStateEquivModPi :
+    plusState ≈zx plusStateTwoPi := by
+    rw [ZX.Equiv]
+    use 1
+    norm_num
+    intro g
+    simp only [ZX.sem, zSpiderSem]
+    push_cast
+    simp
