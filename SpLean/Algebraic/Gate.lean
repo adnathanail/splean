6270243cb7e36a04967812e5b7234114653c398b
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-08-30 20:53:39 +0200

    Add nth_zx_rw tactic

--- SpLean/Algebraic/Gate.lean
+++ SpLean/Algebraic/Gate.lean
@@ -5,6 +5,7 @@ open SpLean.Algebraic
 namespace SpLean.Algebraic.Gate
 
 abbrev I : ZX 1 1 := .spider .Z 1 1
+abbrev I_X : ZX 1 1 := .spider .X 1 1
 
 abbrev T : ZX 1 1 := .spider .Z 1 1 (π/4)
 abbrev S : ZX 1 1 := .spider .Z 1 1 (π/2)
